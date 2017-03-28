fc95c850b59282c3d3fff97368d35ebf5a741a94
Paul Yang <shohokuooo@gmail.com>
2017-03-28 22:50:18 +0800

    Add data from kaggle Titanic

---
+++ data/kaggle/HxyTitanicDataProc.py
@@ -0,0 +1,47 @@
+# -*- coding: cp936 -*-
+import pandas as pd #数据分析
+import numpy as np #科学计算
+from pandas import Series,DataFrame
+
+data_train = pd.read_csv("Train.csv")
+print data_train.columns
+data_train.info()
+print data_train.describe()
+
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+fig.set(alpha=0.2)  # 设定图表颜色alpha参数
+
+plt.subplot2grid((2,3),(0,0))             # 在一张大图里分列几个小图
+data_train.Survived.value_counts().plot(kind='bar')# plots a bar graph of those who surived vs those who did not. 
+plt.title(u"获救情况 (1为获救)") # puts a title on our graph
+plt.ylabel(u"人数")  
+
+plt.subplot2grid((2,3),(0,1))
+data_train.Pclass.value_counts().plot(kind="bar")
+plt.ylabel(u"人数")
+plt.title(u"乘客等级分布")
+
+plt.subplot2grid((2,3),(0,2))
+plt.scatter(data_train.Survived, data_train.Age)
+plt.ylabel(u"年龄")                         # sets the y axis lable
+plt.grid(b=True, which='major', axis='y') # formats the grid line style of our graphs
+plt.title(u"按年龄看获救分布 (1为获救)")
+
+
+plt.subplot2grid((2,3),(1,0), colspan=2)
+data_train.Age[data_train.Pclass == 1].plot(kind='kde')   # plots a kernel desnsity estimate of the subset of the 1st class passanges's age
+data_train.Age[data_train.Pclass == 2].plot(kind='kde')
+data_train.Age[data_train.Pclass == 3].plot(kind='kde')
+plt.xlabel(u"年龄")# plots an axis lable
+plt.ylabel(u"密度") 
+plt.title(u"各等级的乘客年龄分布")
+plt.legend((u'头等舱', u'2等舱',u'3等舱'),loc='best') # sets our legend for our graph.
+
+
+plt.subplot2grid((2,3),(1,2))
+data_train.Embarked.value_counts().plot(kind='bar')
+plt.title(u"各登船口岸上船人数")
+plt.ylabel(u"人数")  
+plt.show()
