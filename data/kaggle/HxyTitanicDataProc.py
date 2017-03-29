c2b7af6b7aa7bded1321abc4806ef805b3b5285a
Paul Yang <shohokuooo@gmail.com>
2017-03-29 17:50:10 +0800

    Add some train data proc in python file.

--- data/kaggle/HxyTitanicDataProc.py
+++ data/kaggle/HxyTitanicDataProc.py
@@ -10,6 +10,9 @@ print data_train.describe()
 
 
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 fig = plt.figure()
 fig.set(alpha=0.2)  # 设定图表颜色alpha参数
 
@@ -45,3 +48,49 @@ data_train.Embarked.value_counts().plot(kind='bar')
 plt.title(u"各登船口岸上船人数")
 plt.ylabel(u"人数")  
 plt.show()
+
+#ticket是船票编号，应该是unique的，和最后的结果没有太大的关系，不纳入考虑的特征范畴
+#cabin只有204个乘客有值，我们先看看它的一个分布
+print data_train.Cabin.value_counts()
+
+from sklearn.ensemble import RandomForestRegressor
+
+
+### 使用 RandomForestClassifier 填补缺失的年龄属性
+def set_missing_ages(df):
+    # 把已有的数值型特征取出来丢进Random Forest Regressor中
+    age_df = df[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
+
+    # 乘客分成已知年龄和未知年龄两部分
+    known_age = age_df[age_df.Age.notnull()].as_matrix()
+    unknown_age = age_df[age_df.Age.isnull()].as_matrix()
+
+    # y即目标年龄
+    y = known_age[:, 0]
+
+    # X即特征属性值
+    X = known_age[:, 1:]
+
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    rfr.fit(X, y)
+
+    # 用得到的模型进行未知年龄结果预测
+    predictedAges = rfr.predict(unknown_age[:, 1::])
+
+    # 用得到的预测结果填补原缺失数据
+    df.loc[(df.Age.isnull()), 'Age'] = predictedAges
+
+    return df, rfr
+
+
+def set_Cabin_type(df):
+    df.loc[(df.Cabin.notnull()), 'Cabin'] = "Yes"
+    df.loc[(df.Cabin.isnull()), 'Cabin'] = "No"
+    return df
+
+
+data_train, rfr = set_missing_ages(data_train)
+data_train = set_Cabin_type(data_train)
+print data_train
+data_train.info()
